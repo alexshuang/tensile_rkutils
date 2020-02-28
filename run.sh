64ffa2d918f8b6ede62b8a28e9f3dac20b1362e1
Alex Huang <nikshuang@sina.com>
2020-02-28 01:13:09 -0500

    add bf16 yaml

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-RUNDIR=~/tony/MT64x128x32/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/source/assembly
+#RUNDIR=~/tony/MT64x128x32/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/source/assembly
+RUNDIR=bf16_out/1_BenchmarkProblems/Cijk_Alik_Bljk_BBH_00/00_Final/source/assembly/
 
-rkgen -f config/MT64x128x32.json
-cp MT64x128x32.sp3 ${RUNDIR} -v
+rkgen -f config/bf16_MT64x128x32.json
+cp bf16_MT64x128x32.sp3 ${RUNDIR} -v
 cd ${RUNDIR}
-sp3cc MT64x128x32.sp3 && rkbuild MT64x128x32.inc
+sp3cc bf16_MT64x128x32.sp3 && rkbuild bf16_MT64x128x32.inc
